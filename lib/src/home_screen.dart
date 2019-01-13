94a6dc30c1e20833366ea9d724e3d3689c10db09
Raveesh Agarwal <raveeshr503@gmail.com>
2019-01-13 16:44:39 +0530

    coloredCard => coloredContainer

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -1,19 +1,19 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-//TODO: 01 We begin with a CustomScrollView, randomColor, and coloredCard
+//TODO: 01 We begin with a CustomScrollView, randomColor, and coloredContainer
 class HomeScreen extends StatelessWidget {
   final Random _random = Random(56);
   Color get randomColor => Color(0xFF000000 + _random.nextInt(0x00FFFFFF));
 
-  Widget coloredCard(Color color) => Container(
+  Widget coloredContainer(Color color) => Container(
         color: color,
       );
 
   @override
   Widget build(BuildContext context) {
     List<Widget> scrollItems =
-        List.generate(52, (int index) => coloredCard(randomColor));
+        List.generate(52, (int index) => coloredContainer(randomColor));
 
     return Scaffold(
       body: CustomScrollView(
